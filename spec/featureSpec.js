044a6a4f366549e0ed9a13582d1cc290442a42a5
Chris Mitchell <chrisjmit@gmail.com>
2017-06-17 11:40:01 +0100

    Feature test added for players to take turns until the game is over

--- spec/featureSpec.js
+++ spec/featureSpec.js
@@ -7,12 +7,30 @@ describe("Feature Test:", function() {
     game = new Game();
     player1 = new Player();
     player2 = new Player();
-  });
-
-  it(' There are two players in the game', function() {
     game.addPlayer(player1);
     game.addPlayer(player2);
+  });
+
+  it("There are two players in the game", function() {
     expect(game.players).toEqual([player1, player2]);
   });
 
+  it("Players take turns until the game is over", function() {
+    game.play('x', 0, 0);
+    game.play('o', 1, 1);
+    game.play('x', 2, 1);
+    game.play('o', 0, 2);
+    game.play('x', 2, 0);
+    game.play('o', 1, 0);
+    game.play('x', 1, 2);
+    game.play('o', 2, 2);
+    game.play('x', 0, 1);
+    expect(function(){ game.play('o', 2, 2); }).toThrowError("All turns taken");
+  });
+
 });
+
+//   0 1 2
+// 0 x o x
+// 1   o x
+// 2 o x
